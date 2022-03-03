752e013eeed5f9defa49f7ef89b9459ca993f7d0
Markuss Grinhāgens <markussgr@gmail.com>
2022-03-03 15:58:30 +0200

    Edit: Professor variables

---
+++ models/ProfDegree.java
@@ -0,0 +1,5 @@
+package SEMINARS_2.models;
+
+public enum ProfDegree {
+    bachelor, master, doctor, notknown
+}
---
+++ models/Professor.java
@@ -0,0 +1,7 @@
+package SEMINARS_2.models;
+
+public class Professor {
+    private String name;
+    private String surname;
+    private String degree;
+}
