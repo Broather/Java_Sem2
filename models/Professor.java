b54a231c1d4f5209ba485654a571404646e09019
Markuss Grinhāgens <markussgr@gmail.com>
2022-03-03 16:03:56 +0200

    Edit: Added get and set functions

--- models/Professor.java
+++ models/Professor.java
@@ -3,5 +3,31 @@ package SEMINARS_2.models;
 public class Professor {
     private String name;
     private String surname;
-    private String degree;
+    // zinam visas visas vērtības kādas degree var būt
+    private ProfDegree degree;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public ProfDegree getDegree() {
+        return degree;
+    }
+
+    public void setDegree(ProfDegree degree) {
+        this.degree = degree;
+    }
+
 }
