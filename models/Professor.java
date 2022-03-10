aca877eb8ecd576ee88ce753dfb33d1773e0f2a3
Markuss Grinhāgens <markussgr@gmail.com>
2022-03-10 13:46:02 +0200

    Edit: added regex and null check for set functions

--- models/Professor.java
+++ models/Professor.java
@@ -3,36 +3,46 @@ package SEMINARS_2.models;
 public class Professor {
     private String name;
     private String surname;
-    // zinam visas visas vērtības kādas degree var būt
+    // zinam visas visas vērtības kādas degree var būt, tāpēc lietojam enum
     private ProfDegree degree;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        if (name.matches("")) {
-
-            this.name = name;
-        } else {
-
-        }
-    }
-
     public String getSurname() {
         return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public ProfDegree getDegree() {
         return degree;
     }
 
+    public void setName(String name) {
+        // regex priekš Vārds Uzvārds
+        if (name != null && name.matches("([A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[a-zēūīļķģšāžčņ]+\\s?){1,2}")) {
+
+            this.name = name;
+        } else {
+            this.name = "notknown";
+        }
+    }
+
+    public void setSurname(String surname) {
+        if (surname != null && surname.matches("([A-ZĒŪĪĀŠĢĶĻŅČŽ]{1}[a-zēūīļķģšāžčņ]+\\s?){1,2}")) {
+            this.surname = surname;
+
+        } else {
+            this.surname = "notknown";
+        }
+    }
+
     public void setDegree(ProfDegree degree) {
-        this.degree = degree;
+        if (degree != null) {
+            this.degree = degree;
+        } else {
+            this.degree = ProfDegree.notknown;
+        }
     }
 
 }
