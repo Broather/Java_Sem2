d862bc591ca174f72d8bf5e8e4666205a645c725
Markuss Grinhāgens <markussgr@gmail.com>
2022-03-10 15:56:17 +0200

    Edit: added setters and getters with validation for setters

--- models/Grade.java
+++ models/Grade.java
@@ -1,8 +1,55 @@
 package SEMINARS_2.models;
 
 public class Grade {
+    private static int idCounter = 200000;
+
     private int id;
     private int gradeValue;
     private Student student;
     private Course course;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getGradeValue() {
+        return gradeValue;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setId(int id) {
+        this.id = idCounter++;
+    }
+
+    public void setGradeValue(int gradeValue) {
+        if (gradeValue >= 1 && gradeValue <= 10) {
+            this.gradeValue = gradeValue;
+        } else {
+            this.gradeValue = -1;
+        }
+    }
+
+    public void setStudent(Student student) {
+        if (student != null) {
+            this.student = student;
+        } else {
+            this.student = new Student();
+        }
+    }
+
+    public void setCourse(Course course) {
+        if (course != null) {
+            this.course = course;
+        } else {
+            this.course = new Course();
+        }
+    }
+
 }
